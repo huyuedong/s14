20dd390ad11f3a88c66a4cdb88ee2e4e6a515da4
huyuedong <huyuedong163@163.com>
2016-08-10 18:14:38 +0800

    huyuedong

--- 03/work01/open.py
+++ 03/work01/open.py
@@ -17,16 +17,64 @@ def fetch(backend):
                 break
             if flag and line:
                 record_list.append(line)
-                for i in record_list:
-                    print(i)
-                    exit()
-    # return record_list
+    return record_list
+
+def add(dict_info):
+    backend = dict_info.get("backend")
+    backend_title = "backend %s" % backend
+    record_list = fetch(backend)
+    current_record = "server %s %s weight %d maxconn %d" % (dict_info['record']['server'],dict_info['record']['server'],dict_info['record']['weight'],dict_info['record']['maxconn'])
+    if not record_list:
+        record_list.append(backend_title)
+        record_list.append(current_record)
+        with open('haproxy.conf') as read_file,open('haproxy.new','w') as write_file:
+            flag = False
+            for line in read_file:
+                write_file.write(line)
+            for i in record_list:
+                if i.startswith('backend'):
+                    write_file.write(i+'\n')
+                else:
+                    write_file.write("%s%s\n" % (8*" ",i))
+    else:
+        record_list.insert(0,backend_title)
+        if current_record not in record_list:
+            record_list.append(current_record)
+
+        with open('haproxy.conf') as read_file,open('haproxy.new','w') as write_file:
+            flag = False
+            has_write = False
+            for line in read_file:
+                line_strip = line.strip()
+                if line_strip == backend_title:
+                    flag = True
+                    continue
+                if flag and line_strip.startswith('backend'):
+                    flag = False
+                if not flag:
+                    write_file.write(line)
+                else:
+                    if not has_write:
+                        for i in record_list:
+                            if i.startswith('backend'):
+                                write_file.write(i+'\n')
+                            else:
+                                write_file.write("%s%s\n" % (8*" ", i))
+                    has_write = True
+    os.rename('haproxy.conf','haproxy.conf.bak')
+    os.rename('haproxy.new','haproxy.conf')
+
+
 
 if __name__ == '__main__':
     print('1.获取；2.添加；3.删除')
     num = input('请输入序号：')
     data = input('请输入内容：')
     if num == '1':
-        fetch(data)
+        record_list = fetch(data)
+        for i in record_list:
+            print(i)
+            exit()
     else:
-        pass
+        dict_data = json.loads(data)
+        add(dict_data)
---
+++ 03/work01/test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+#-*-coding:utf-8-*-
+import json
+
+str = input("input:")
+data = json.load(str)
+print(data)
